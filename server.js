9243fb14ee35a60215f67df7c8623066f097a1b2
DonGroovy1120 <groovyrocome@gmail.com>
2023-07-23 22:17:07 -0400

    12

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var port = process.env.PORT || 8080;
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
-var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
 var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
 function parseEnvList(env) {
   if (!env) {
