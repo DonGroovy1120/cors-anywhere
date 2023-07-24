637560c561ebafe8bf84ebe52ff105d519948012
DonGroovy1120 <groovyrocome@gmail.com>
2023-07-23 21:45:08 -0400

    server

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
-  originBlacklist: originBlacklist,
+  originBlacklist: null,
   originWhitelist: originWhitelist,
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
